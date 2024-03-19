9396a5b3bd2b4912e58a9aec0c6c78df1f0a47d2
Vera Angerer <vera.angerer@redbull.com>
2024-03-19 19:07:10 +0100

    test: fix the sum functionality

--- desktop-app/src/utils/index.ts
+++ desktop-app/src/utils/index.ts
@@ -1,4 +1,8 @@
 // @ts-ignore
 export function sum(a: number, b: number, c: number | undefined = undefined) {
-  return a + b;
+  if (c === undefined) {
+    return a + b;
+  }
+
+  return a + b + c;
 }
